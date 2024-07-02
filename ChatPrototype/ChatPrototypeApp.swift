5531cf39edbd9613cd16644bbefaaf93ae5fa1bb
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-07-02 23:17:27 +0900

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by 河野礼嗣 on 2024/07/02.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
