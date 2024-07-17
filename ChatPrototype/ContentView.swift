b0cf7690f3cec3103435f325fadce1da51df4387
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-07-17 20:41:44 +0900

    aaa

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -7,13 +7,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 6) {
             Text("コンコン")
                 .padding()
                 .background(Color.green, in: RoundedRectangle(cornerRadius: 8))
             Text("誰かいますか？")
                 .padding()
                 .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+            HStack(spacing: 4, content: {
+                Text("みつけてごらん").padding().background(Color.gray, in:RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+                Text("ここにいるよ").padding().background(Color.pink, in:RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+            })
         }
         .padding()
     }
